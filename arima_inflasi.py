19c2d39c4ce378bb20f8fdcf659f6fe1ab661760
nasriaw <nasriaw@gmail.com>
2025-04-14 05:34:45 +0700

    Update arima_inflasi.py

--- arima_inflasi.py
+++ arima_inflasi.py
@@ -2,18 +2,18 @@
 import streamlit as st
 import pingouin as pg
 import pandas as pd
-import matplotlib.pyplot as plt
-from pandas_datareader import data as pdr
 import numpy as np
-import plotly.express as px
 import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+from pandas_datareader import data as pdr
 from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.tsa.stattools import adfuller
-import pmdarima
 from pmdarima import auto_arima
+import statistics
 import warnings
 import time
 from datetime import timedelta
@@ -21,88 +21,76 @@ warnings.filterwarnings('ignore')
 
 features=[
     "Introduksi",
-    "Upload File",
     "Diskripsi",
     "Plot historical inflation",
     "Check stationarity and autocorrelation",
-    "Fit SARIMA model, parameter, prediksi dan evaluasi",
+    "SARIMAX model, Forecast, Evaluation model & Predict",
     ]
 menu=pd.DataFrame(features)
 #st.write(menu)
 #[m,n] =menu.shape
 #st.write(m,n)
 #st.sidebar.image("logo_stiei.jpg", use_column_width=False)
-st.sidebar.markdown('<h3 style="color: Blue;"> Author: Nasri </h3>', unsafe_allow_html=True)
-st.sidebar.markdown('<h5 style="color: Blue;"> email: nasri@stieimlg.ac.id </h5>', unsafe_allow_html=True)
-st.sidebar.markdown('<h1 style="color: Blue;">Prediksi Inflasi dengan model SARIMA</h1>', unsafe_allow_html=True)
+st.sidebar.markdown('<h3 style="color: White;"> Author: Nasri </h3>', unsafe_allow_html=True)
+st.sidebar.markdown('<h5 style="color: White;"> email: nasri@stieimlg.ac.id </h5>', unsafe_allow_html=True)
+st.sidebar.markdown('<h1 style="color: White;">Analisis Prediksi Inflasi Harga dengan model SARIMAX</h1>', unsafe_allow_html=True)
 
-model_analisis = st.sidebar.radio('Baca ketentuan penggunaan dengan seksama, Pilih Analisis Statistik:', menu)
+model_analisis = st.sidebar.radio('Baca ketentuan penggunaan dengan seksama, Pilih Analisis Prediksi Inflasi Harga:', menu)
 
 def intro():
-    st.write("## Selamat Datang di Dashboard Prediksi Inflasi di Indonesia.  👋  👋")
+    st.write("## Selamat Datang di Dashboard Analisis Prediksi Inflasi Harga di Indonesia, Menggunakan Model SARIMAX.  👋  👋")
     st.write("##### author: m nasri aw, email: nasri@stieimlg.ac.id; lecturer at https://www.stieimlg.ac.id/; Des 2024.")
-    st.write(f"##### - Ketentuan: ")
+    st.write(f"##### - Pendekatan analisis: ")
     '''
     1. Aplikasi ini menggunakan bahasa python dengan library utama statsmodels dan streamlit.
-    2. Menggunakan model SARIMA dengan data training inflasi 18 Tahun @ 12 bulan, model dapat digunakan untuk prediksi inflasi.
-    3. Diperlukan data dalam bentuk file csv (contoh dapat di download di https://github.com/nasriaw/inflation_indonesia_2006-2024/blob/main/inflasi_harga_konsumen_nasional_bulanan_2006_2024.csv; Upload File pada menu sebelah 👈.
-    4. File hanya terdiri dari kolom pertama kolom waktu (bersifat series bisa dalam bentuk bulanan dan musiman), kolom ke dua data inflasi atau sejenis, misal data perjalanan, penenan, dll.
-    5. Analisis statistik regresi meliputi:
+    2. Menggunakan model SARIMAX dengan data series (musiman) sebagai data training.
+    3. File hanya terdiri dari kolom pertama kolom waktu (bersifat series bisa dalam bentuk bulanan dan musiman), kolom ke dua data inflasi atau sejenis, misal data perjalanan, penenan, dll.
+    4. Diperlukan data dalam bentuk file csv, sebagai contoh disini menggunakan data inflasi harga di Indonesia kurun 2006-2024 (sumber bps, 2025), untuk file csv dapat di download di https://github.com/nasriaw/inflation_indonesia_2006-2024/blob/main/inflasi_harga_konsumen_nasional_bulanan_2006_2024.csv;
+    5. Analisis data series prediksi inflasi ini meliputi:
        1. Diskripsi.
        2. Plot historical inflation.
        3. Check stationarity and autocorrelation.
-       4. Fit SARIMA model, parameter, prediksi dan evaluasi.
-       ###### 👈 Pilih Menu di sebelah; Pastikan data telah di upload (langkah ke-2: Upload File)
+       4. SARIMA model, Forecast, Evaluation model & Predict.
+       ###### 👈 Pilih Menu di sebelah.
+    6. Untuk link demo silahkan klik https://huggingface.co/spaces; Selamat belajar semoga memudahkan untuk memahami statistik regresi.
     '''
     return intro
 
-def open_file():
-    if 'data' not in st.session_state:
-        st.session_state.data = None
-
-    def load_data():
-        st.session_state.data = pd.read_csv(st.session_state.loader)
-
-    file = st.file_uploader('Choose a file', type='csv', key='loader', on_change=load_data)
-
-    df = st.session_state.data
-    if df is not None:
-        # Run program
-        st.write('Gunakan Browse Files jika upload data baru.')
-    return df
-#df=open_file()
-
 def descriptive():
-    df=open_file()
+    #df=open_file()
+    df=pd.read_csv('inflasi_harga_konsumen_nasional_bulanan_2006_2024.csv')
     df= df.dropna()
     df.iloc[:,0]=pd.to_datetime(df.iloc[:,0])
     st.write("### 1. Data Head dan Statistik Diskripsi ") #{df.iloc[:,1]}")
-    st.write(f"dimensi data: {df.shape}")
-    st.write("Data Head : ")
+    st.write(f"#### dimensi data: {df.shape}")
+    st.write("#### Data Head : ")
     st.write(df.head())
-    st.write("Data Diskripsi Inflasi : ")
+    st.write("#### Data Diskripsi Inflasi : ")
     st.write(df.iloc[:,1].describe())#(include='all').fillna("").astype("str"))
     
 def historical():
-    df=open_file()
+    #df=open_file()
+    df=pd.read_csv('inflasi_harga_konsumen_nasional_bulanan_2006_2024.csv')
     df.set_index(df.iloc[:,0], inplace=True)
-    st.line_chart(df.iloc[:,1]) #scatter, bar, line, area, altair
+    st.write("### Inflation Data (2006-2024) Line Chart")
+    st.line_chart(df.iloc[:,1], x_label="bulan", y_label="inflation, %") #scatter, bar, line, area, altair
 
 def check_stationarity():
-    # S3: Check stationarity and autocorrelation
-    # Plot ACF and PACF to identify ARIMA parameters
-    df=open_file()
+    # Check stationarity and autocorrelation
+    # Plot ACF and PACF, given ARIMA parameters
+    df=pd.read_csv('inflasi_harga_konsumen_nasional_bulanan_2006_2024.csv')
     df.set_index(df.iloc[:,0], inplace=True)
     
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 4))
+    st.write("### Auto Correlation Chart")
+    fig, (ax1) = plt.subplots(1, 1, figsize=(8, 3))
     plot_acf(df.iloc[:,1], ax=ax1)
-    plot_pacf(df.iloc[:,1], ax=ax2)
     st.pyplot(fig)
+        
+    fig1, (ax2) = plt.subplots(1, 1, figsize=(8, 3))
+    plot_pacf(df.iloc[:,1], ax=ax2)
+    st.pyplot(fig1)
     
-    #decomposition_montly = seasonal_decompose(df.iloc[:,1], model='additive', period=12)
-    #decomposition_montly.plot()
-    #st.pyplot(fig)
- 
+    st.write("### ADF, P-value Result")
     result = adfuller(df.iloc[:,1])
     st.write("ADF Statistic:", result[0].round(4))
     st.write("p-value:", result[1].round(4))
@@ -116,43 +104,27 @@ def check_stationarity():
         meaning it exhibits trends or seasonality and does not have constant mean (constant??) and variance over time.
     '''
 
-def SARIMA_model():
-    df=open_file()
+def SARIMAX_model():
+    df=pd.read_csv('inflasi_harga_konsumen_nasional_bulanan_2006_2024.csv')
     df.set_index(df.iloc[:,0], inplace=True)
-    
     start_time = time.time()
-    with st.spinner("Tunggu proses optimasi parameter SARIMA", show_time=True):
-        model = auto_arima(df.iloc[:,1], seasonal=True, m=12, trace=True, error_action='ignore', suppress_warnings=True)
-        model.fit(df.iloc[:,1])
+    with st.spinner("Tunggu proses Optimized Model SARIMAX", show_time=True):
+        optimized_model = SARIMAX(
+            df.iloc[:,1],  
+            order=(5,1,1), #model.order[:3], Non-seasonal parameters
+            seasonal_order=(1, 0, 1, 12), #model.seasonal_order[:4], Seasonal parameters
+            enforce_stationarity=True, # False if p>0.05,
+            enforce_invertibility=False
+        )
         end_time = time.time()
         time.sleep=end_time
         time_lapsed =np.mean(end_time - start_time)
-        #time_process=float(time_lapsed.timedelta(seconds=666))
         st.success(f"Selesai !!, waktu optimasi parameter SARIMA : {str(timedelta(seconds=time_lapsed))} detik': ")
-           
-    st.write("Optimal parameter : ")
-    st.write(model)
-    st.write(f'parameter order optimal, p,d,q : {model.order}')
-    st.write(f'parameter seasonal order optimal P,D,Q,m : {model.seasonal_order}')
-    #order=model.order
-    #seasonal_order=model.seasonal_order
-    #return (model.order, model.seasonal_order)
-    #df.set_index(df.iloc[:,0], inplace=True)
-    st.write("Output Optimized model SARIMA : ")
-    optimized_model = SARIMAX(
-        df.iloc[:,1],  
-        order=model.order[:3], #(5, 1, 1),              # Non-seasonal parameters
-        seasonal_order=model.seasonal_order[:4], #(1, 0, 1, 12),  # Seasonal parameters
-        enforce_stationarity=True, # False if p>0.05,
-        enforce_invertibility=False
-    )
- 
+            
     optimized_sarima_fit = optimized_model.fit(disp=False)
+    st.write("#### telah dihitung untuk pola data inflasi, menggunakan  auto_arima, diperoleh paramater order:(5,1,1), seasonal_order:(1,0,1,12)")
+    st.write("### SARIMAX RESULT")
     st.write(optimized_sarima_fit.summary())
-    
-#    optimized_sarima_fit.plot_diagnostics(figsize=(12, 8))
-#    st.pyplot(fig)
-#    return optimized_sarima_fit
     
     train = df.iloc[:-24] 
     test = df.iloc[-24:]
@@ -161,23 +133,14 @@ def SARIMA_model():
     
     forecast_test_optimized = optimized_sarima_fit.forecast(steps=24)
     forecast_test_optimized.index = test.index
-
-    st.line_chart(train.iloc[:,1], x_label="bulan", y_label="inflasi")
-    st.line_chart(test.iloc[:,1],x_label="bulan", y_label="inflasi")
-    st.line_chart(forecast_test_optimized, x_label="bulan", y_label="inflasi Forecast")
     
-    #fig1, ax1 = plt.subplots(1, 1, figsize=(8, 3))
-    #ax1.plot(train.iloc[:,1],label="Train Data", color="black")
-    #ax1.set_title('Train Data Chart')
-    #st.pyplot(fig1)
+    st.write("### Data Train Line Chart")
+    st.line_chart(train.iloc[:,1], x_label="bulan, train", y_label="inflasi, %")
+    st.write("### Test Line Chart")
+    st.line_chart(test.iloc[:,1],x_label="bulan, testing", y_label="inflasi testing, %")
+    st.write("### Forecast Line Chart")
+    st.line_chart(forecast_test_optimized, x_label="bulan forecast", y_label="inflasi Forecast, %")
     
-    # fig2, (ax2, ax3) = plt.subplots(2, 1, figsize=(8, 6))
-    # ax2.plot(test.iloc[:,1], label="Test Data", color="blue")
-    # ax2.set_title('Test Data Chart')
-    # ax3.plot(forecast_test_optimized, label="Forecast", color="red")
-    # ax3.set_title('Forecast Data Chart')
-    # st.pyplot(fig2)
-
     predicted_values = forecast_test_optimized.values 
     actual_values = test.iloc[:,1] #values #.flatten()
 
@@ -187,11 +150,24 @@ def SARIMA_model():
         'deviation': predicted_values-actual_values.round(4),
         'deviation^2': (predicted_values-actual_values.round(4))**2
     })
+    st.write("### Actual - Predicted - Deviation")
     st.write(prediction_df)
+    
+    data = {
+        "Actual Inflation": actual_values,
+        "Predicted Inflation": predicted_values,}
+    df = pd.DataFrame(data) #, index=dates)
+    column_names=list(df)
+    #column_names[0]
+    #column_names[1]
+    
+    st.write("### Actual - Predicted Line Chart")
+    st.line_chart(df, x_label="bulan", y_label="inflation, %") #.iloc[:,1], x_label=column_names[0], y_label=column_names[1])
 
-    # 7. Evaluation Model
+    # Evaluation Model
     # Mean Absolute Error (MAE)
     mae = np.mean(np.abs(predicted_values - actual_values))
+    st.write("### Evaluation Indicator")
     st.write("MAE:", mae.round(4))
 
     # Root Mean Squared Error (RMSE)
@@ -203,20 +179,23 @@ def SARIMA_model():
     # Mean Absolute Percentage Error (MAPE)
     mape = np.mean(np.abs((predicted_values - actual_values) / actual_values)) * 100
     st.write("MAPE:", mape.round(4))
+    
+    # Standard Deviation Predicted
+    std=np.mean(statistics.stdev(predicted_values))
+    st.write(f'Standard Deviation : {(std.round(4))}')
 
-    # 8. Forecast for the next 3 month
+    # Forecast for the next 3 month
+    st.write("### Forecast next 3 month")
     forecast_steps = 3
     forecast, stderr, conf_int = optimized_sarima_fit.forecast(steps=forecast_steps)
 
     # Convert forecast to a pandas Series for easier plotting
     forecast_series = pd.Series(forecast, index=pd.date_range('2025', periods=forecast_steps, freq='ME'))
-    st.write
+    st.write(f'Inflation Predicted next 3 month: {forecast:3f} %')
     
 
 if model_analisis == "Introduksi":
     intro()
-elif model_analisis == "Upload File":
-    open_file()
 elif model_analisis == "Diskripsi":
     descriptive()
 elif model_analisis == "Plot historical inflation":
@@ -224,4 +203,4 @@ elif model_analisis == "Plot historical inflation":
 elif model_analisis == "Check stationarity and autocorrelation":
     check_stationarity()
 else:
-    SARIMA_model()
+    SARIMAX_model()
